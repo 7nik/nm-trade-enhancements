4a2c6d42de22d30e6c3f9c67c58a2504644718fb
7nik <kifiranet@gmail.com>
2022-08-30 21:08:04 +0300

    drop trade support, add notification support, add svelte action export

--- app/enhancements/addTradePreview.ts
+++ app/enhancements/addTradePreview.ts
@@ -1,13 +1,17 @@
 import type NM from "../utils/NMTypes";
-import type Services from "../utils/NMServices";
 import type { CreateSingletonInstance, CreateSingletonProps, Instance, Props } from "tippy.js";
 
 import tippy, { createSingleton } from "tippy.js";
-import cardsInTrades, { getTrades } from "../utils/cardsInTrades";
+import { getTrades, type GetTrades } from "../utils/cardsInTrades";
 import addPatches from "../utils/patchAngular";
-import { forAllElements, getScope } from "../utils/utils";
 import NMApi from "../utils/NMApi";
 import TradePreviews from "../components/TradePreviews.svelte";
+
+declare global {
+    interface HTMLElement {
+        _tippy?: Instance
+    }
+}
 
 /**
  * Adds a trade preview on hovering to an element
@@ -16,10 +20,6 @@ import TradePreviews from "../components/TradePreviews.svelte";
  * @param cardId - a card to highlight in the trades
  */
 async function attachTip (elem: HTMLElement, tradeIds: number[], cardId?: number) {
-    // destroy previous tip if presented
-    // @ts-ignore
-    // eslint-disable-next-line no-underscore-dangle
-    elem._tippy?.destroy();
     if (tradeIds.length === 0) return;
 
     const trades = await Promise.all(tradeIds.map((id) => NMApi.trade.get(id)));
@@ -31,58 +31,102 @@ async function attachTip (elem: HTMLElement, tradeIds: number[], cardId?: number
             trades,
             highlightCardId: cardId,
         },
-    // @ts-ignore
     }).$on("click", () => elem._tippy?.hide());
 
+    // destroy previous tip if presented
+    // eslint-disable-next-line no-underscore-dangle
+    elem._tippy?.destroy();
     tippy(elem, {
         appendTo: document.body,
-        delay: [500, 200],
+        delay: [350, 200],
         interactive: true,
         content: tip,
         theme: "trade",
     });
 }
 
-type Scope = angular.IScope & {
-    print?: NM.PrintInTrade,
-    piece?: NM.PrintInTrade,
+type CardScope = angular.IScope & {
+    piece: NM.PrintInTrade,
     trades: number[],
 }
+
+type NotificationScope = angular.IScope & {
+    notification?: NM.Event<object, string, string>,
+    trade?: NM.Event<object, string, string>,
+}
+
+let currentTradeId = -1;
+let tips: Record<number, Instance<Props>> = {};
+let singleton: CreateSingletonInstance<CreateSingletonProps<Props>>;
 
 /**
  * Shows in which trades the card is used if it is
  */
 addPatches(() => {
-    angular.module("nm.trades").controller("nmUsageInTrades", [
+    angular.module("Art").controller("nmUsageInTrades", [
         "$scope",
         "$element",
-        "nmTrades",
-        ($scope: Scope, $elem, nmTrades: Services.NMTrade) => {
-            const dir = $scope.print // if it's a trade window
-                ? ($elem.closest(".trade--side.trade--you").length > 0 ? "give" : "receive")
-                : "both";
-            const lvl = dir === "give" ? "print" : "card";
-            const currentTrade = $scope.print ? nmTrades.getId() : null;
+        ($scope: CardScope, $elem) => {
             $scope.trades = [];
 
-            async function loadTrades () {
-                let trades = await getTrades($scope.print ?? $scope.piece!, dir, lvl);
-                // exclude the current trade
-                if (trades.length > 0 && currentTrade) {
-                    trades = trades.filter((trade) => trade !== currentTrade);
+            let initialized = false;
+            function loadTrades (trades: GetTrades) {
+                let tradeIds = trades.find($scope.piece, "both", "card");
+                attachTip($elem[0], tradeIds, $scope.piece.id);
+                if (initialized) {
+                    // to notify angular about changes
+                    $scope.$apply(() => { $scope.trades = tradeIds; });
+                } else {
+                    // cannot and need to use $apply during controller initialization
+                    $scope.trades = tradeIds;
                 }
-                attachTip($elem[0], trades, $scope.piece?.id);
-                $scope.$apply(() => { $scope.trades = trades; });
             }
-
-            if ($scope.print) {
-                // when in a trade
-                $scope.$watch(() => $scope.print!.print_id, loadTrades);
-            } else {
-                // when viewing the card
-                $scope.$watch(() => cardsInTrades.lastUpdate, loadTrades);
-            }
+            const unsubscribe = getTrades.subscribe(loadTrades);
+            $scope.$on("$destroy", unsubscribe);
+            initialized = true;
         },
+    ]);
+    angular.module("Art").controller("tradePreview", [
+        "$scope",
+        "$element",
+        (scope: NotificationScope, $elem: JQLite) => {
+            if ((scope.notification ?? scope.trade)?.object.type !== "trade-event") return;
+            const tradeId = ((scope.notification ?? scope.trade) as NM.TradeEvent).object.id;
+            // the tip is bound to the element,
+            // so create new tip for new element (notification)
+            const tip = tippy($elem[0], {
+                onTrigger: async (instance) => {
+                    if (instance.props.content) return;
+                    currentTradeId = tradeId;
+                    const preview = document.createElement("div");
+                    new TradePreviews({
+                        target: preview,
+                        props: {
+                            trades: [await NMApi.trade.get(tradeId)],
+                            showButton: false
+                        },
+                    });
+                    // set only if it is still actual
+                    if (currentTradeId === tradeId) {
+                        instance.setContent(preview);
+                    }
+                },
+                onShow: () => { document.body.contains($elem[0]); },
+            });
+            // and override the previous tips if there are any
+            tips[tradeId] = tip;
+            // if it's the first hovered notification ever
+            if (!singleton) {
+                singleton = createSingleton([tip], {
+                    delay: [600, 200],
+                    placement: "left",
+                    theme: "trade sidebar",
+                    overrides: ["onTrigger", "onShow"],
+                });
+            } else {
+                singleton.setInstances(Object.values(tips));
+            }
+        }
     ]);
 }, {
     // insert card usage on collection page
@@ -105,73 +149,33 @@ addPatches(() => {
             class="card-trading-icon"></span>`,
     }],
 }, {
-    // insert card usage in trades
-    names: [
-        "/static/common/trades/partial/add.html",
-        "/static/common/trades/partial/item-list.html",
-    ],
+    names: ["partials/art/notifications/notification-center.partial.html"],
     patches: [{
-        target: "</li>",
-        prepend: `
-            <span data-ng-controller="nmUsageInTrades" class="card-trading">
-                <span ng-if="trades.length === 1" class="text-warning">
-                    Used in another trade
-                </span>
-                <span ng-if="trades.length > 1" class="text-warning">
-                    Used in {{trades.length}} more trades
-                </span>
-            </span>`,
+        // add trade preview for notifications
+        target: "art-notification=",
+        prepend: `data-ng-controller="tradePreview" `
+    }, {
+        // add trade preview for active trades
+        target: `ng-repeat="trade in state.trades track by trade.id"`,
+        append: ` data-ng-controller="tradePreview"`
     }],
 });
 
-let currentTradeId = -1;
-let tips: Record<number, Instance<Props>> = {};
-let singleton: CreateSingletonInstance<CreateSingletonProps<Props>>;
-
 /**
- * Allows to add previews to pending trades and trade notifications in the sidebar
- * @param {HTMLElement} notification - <li.nm-notification> or <li.nm-notifications-feed--item>
+ * Svelte action that adds a trade preview on hovering to an element
+ * @param elem - the element to attach the trade preview
+ * @param param.tradeIds - the trade ids to preview
+ * @param param.cardId - a card to highlight in the trades
  */
-async function addTradePreview (notification: HTMLElement) {
-    const scope = getScope<{ notification: NM.TradeEvent, trade: NM.TradeEvent }>(notification);
-    if (scope.notification && scope.notification.object.type !== "trade-event") return;
-    const tradeId = (scope.notification ?? scope.trade).object.id;
-    // the tip is bound to the element,
-    // so create new tip for new element (notification)
-    const tip = tippy(notification, {
-        onTrigger: async (instance) => {
-            if (instance.props.content) return;
-            currentTradeId = tradeId;
-            const preview = document.createElement("div");
-            new TradePreviews({
-                target: preview,
-                props: {
-                    trades: [await NMApi.trade.get(tradeId)],
-                    showButton: false
-                },
-            });
-            // set only if it is still actual
-            if (currentTradeId === tradeId) {
-                instance.setContent(preview);
-            }
+export default function (elem: HTMLElement, { tradeIds, cardId }: { tradeIds: number[], cardId?: number }) {
+    attachTip(elem, tradeIds, cardId);
+    return {
+        update({ tradeIds, cardId }: { tradeIds: number[], cardId?: number }) {
+            elem._tippy?.destroy();
+            attachTip(elem, tradeIds, cardId);
         },
-        onShow: () => document.body.contains(notification) && void 0,
-    });
-    // and override the previous tips if there are any
-    tips[tradeId] = tip;
-    // if it's the first hovered notification ever
-    if (!singleton) {
-        singleton = createSingleton([tip], {
-            delay: [600, 200],
-            placement: "left",
-            theme: "trade sidebar",
-            overrides: ["onTrigger", "onShow"],
-        });
-    } else {
-        singleton.setInstances(Object.values(tips));
+        destroy() {
+            elem._tippy?.destroy();
+        }
     }
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-    forAllElements(document, "li.nm-notification, li.nm-notifications-feed--item", addTradePreview);
-});
