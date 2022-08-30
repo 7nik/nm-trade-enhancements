dd2c3a536fc9f3c4ecd5f32c2acc197ae79d3970
7nik <kifiranet@gmail.com>
2022-08-30 21:12:43 +0300

    move the login into angular

--- app/enhancements/addLastActionAgo.ts
+++ app/enhancements/addLastActionAgo.ts
@@ -1,36 +1,32 @@
-import { MutationSummary } from "mutation-summary";
 import NMApi from "../utils/NMApi";
-import { forAllElements, getScope } from "../utils/utils";
+import addPatches from "../utils/patchAngular";
 
-
-/**
- * Adds how ago was last action of the user
- * @param {HTMLElement} header - <div.nm-conversation--header>
- */
-async function addLastActionAgo (header: HTMLElement, watchForChanges = true) {
-    // TODO: move the logic to angular
-    const userId: number = getScope<any>(header).recipient.id;
-    const actions = await NMApi.user.activityFeed(userId);
-    const lastActionAgo = actions[0]?.created ?? "one eternity ago";
-
-    const div = document.createElement("div");
-    div.className = "last-action";
-    div.innerHTML = `last action: <i>${lastActionAgo}</i>`;
-    const container = header.querySelector(".nm-conversation--header h3")!;
-    container.append(div);
-
-    if (watchForChanges === false) return;
-
-    new MutationSummary({
-        rootNode: container.querySelector("a")!,
-        queries: [{ characterData: true }],
-        callback: () => {
-            header.querySelector(".last-action")?.remove();
-            addLastActionAgo(header, false);
-        },
-    });
+type Scope = angular.IScope & {
+    userId: number,
+    lastActionAgo: string,
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    forAllElements(document, "div.nm-conversation--header", addLastActionAgo);
+function setLastActionAgo(scope: Scope) {
+    scope.lastActionAgo = "...";
+    NMApi.user.activityFeed(scope.userId).then((actions) => {
+        const lastActionAgo = actions[0]?.created ?? "one eternity ago";
+        scope.$apply(() => scope.lastActionAgo = lastActionAgo);
+    }); 
+}
+
+addPatches(() => {
+    angular.module("Art").directive("lastAction", [() => ({
+        scope: { userId: "=lastAction"},
+        link: (scope: Scope) => {
+            setLastActionAgo(scope);
+            scope.$watch("userId", () => setLastActionAgo(scope));
+        },
+        template: "last action: <i>{{lastActionAgo}}</i>"
+    })]);
+}, {
+    names: ["partials/art/notifications/conversation.partial.html"],
+    patches: [{
+        target: "</h3>",
+        prepend: `<div class="last-action" last-action="recipient.id"></div>`,
+    }]
 });
