a5b7ab86c513de3607b7e527c8b0e718d190edc1
7nik <kifiranet@gmail.com>
2022-09-01 21:02:05 +0300

    add midnight reset of the list of open packs

--- app/enhancements/index.ts
+++ app/enhancements/index.ts
@@ -20,3 +20,13 @@ window.addEventListener("click", (ev) => {
         }
     }
 }, { capture: true });
+
+// reset the list of the opened pack at midnight
+const midnight = new Date();
+midnight.setDate(midnight.getDate() + 1);
+midnight.setHours(0, 0, 0, 0);
+setTimeout(function reset() {
+    NM.you.attributes.todays_freebies_count = {};
+    // reset at the next midnight
+    setTimeout(reset, 24*60*60*1000);
+}, midnight.getTime() - Date.now());
